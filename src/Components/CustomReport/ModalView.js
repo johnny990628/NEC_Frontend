5e555991757a27c263ef0cca783ec2c6ea77197b
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-02-01 02:12:09 +0800

    style: 修改寬度

--- src/Components/CustomReport/ModalView.js
+++ src/Components/CustomReport/ModalView.js
@@ -7,7 +7,7 @@ const style = {
     top: "50%",
     left: "50%",
     transform: 'translate(-50%, -50%)',
-    width: "70%",
+    width: "90%",
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
