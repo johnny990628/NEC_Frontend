44abb4b635a94650523e0823766de86a5a3de811
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-04-30 16:55:46 +0800

    feat: 乳房雷達圖加入方位線

--- src/Components/CustomReport/Component/ChestMarker.js
+++ src/Components/CustomReport/Component/ChestMarker.js
@@ -6,11 +6,23 @@ import { useSelector } from 'react-redux'
 
 function ChestMarker({}) {
     const { report, CHESTMAXSIZE, CHESTMAXRADIUS } = useSelector((state) => state.breast)
+    const lines = Array.from({ length: 12 }).map((_, i) => {
+        const angle = i * 30
+        const x = 200 + Math.sin((angle * Math.PI) / 180) * 200
+        const y = 200 - Math.cos((angle * Math.PI) / 180) * 200
+        return { x, y }
+    })
 
     const Circle = ({ pos, side }) => {
         return (
-            <svg width="400" height="400">
+            <svg width="400" height="400" stroke="black">
                 <circle cx="200" cy="200" r="200" fill="#efefef" />
+                <circle cx="200" cy="200" r="200" fill="#efefef" />
+
+                {lines.map(({ x, y }, i) => (
+                    <line key={i} x1="200" y1="200" x2={x} y2={y} stroke="black" strokeWidth="1" />
+                ))}
+
                 {pos.map(({ id, clock, distance, size }, index) => {
                     const angle = (clock * 360) / 12 + 180
                     const radians = (angle * Math.PI) / 180
