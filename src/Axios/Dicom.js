1bde79f6299070a133f19449eaac2b886459cb6f
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-05-05 23:19:24 +0800

    fix：修正apiDownloadDCM API responseType

--- src/Axios/Dicom.js
+++ src/Axios/Dicom.js
@@ -1,4 +1,4 @@
 import Request from './APIConfig'
 
 export const apiGetDicom = (params) => Request.get('/dicom', { params })
-export const apiDownloadDCM = (studyUID) => Request.get('/dicom/downloadDCM/' + studyUID, { responseType: 'stream' })
+export const apiDownloadDCM = (studyUID) => Request.get('/dicom/downloadDCM/' + studyUID, { responseType: 'arraybuffer' })
