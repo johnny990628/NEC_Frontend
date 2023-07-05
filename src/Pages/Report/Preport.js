98df9ee19a68d08c5ccc104e5923d63705f87844
AdrianoXY <117019832+AdrianoXY@users.noreply.github.com>
2023-07-06 00:10:14 +0800

    Back

--- src/Pages/Report/Preport.js
+++ src/Pages/Report/Preport.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { styled } from '@mui/material/styles'
-import { useDispatch, useSelector } from 'react-redux'
+import { IconButton, ListItemText } from '@mui/material'
+import { Close, Print } from '@mui/icons-material'
+import { Box } from '@mui/system'
 import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
@@ -9,27 +11,21 @@ import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 import InputLabel from '@mui/material/InputLabel'
 import { FormControl, Menu, MenuItem, Select, Typography } from '@mui/material'
-import { fetchSchedule } from '../../Redux/Slices/Schedule'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchReportByReportID } from '../../Redux/Slices/Breast'
+import reportdata from '../../Assets/Json/ReportCols.json'
 import * as AiIcons from 'react-icons/ai'
 
 const Preport = (props) => {
     const { info } = props
+    console.log(info);
     const dispatch = useDispatch()
     const [version, setVersion] = useState('')
-    const [selection, setSelection] = useState({})
+    const [patient, setPatient] = useState(info.patient)
+    const [report, setReport] = useState(reportdata)
     const descriptionElementRef = useRef(null)
     const [anchorEl, setAnchorEl] = useState(null)
     const open = Boolean(anchorEl)
-
-    const Schedule = useSelector((state) => state.schedule)
-    const Report = useSelector((state) => state.breast)
-
-    const HorizonDialogTitle = styled(DialogTitle)({
-        display: 'flex',
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-    })
 
     const HorizonDiv = styled('div')({
         display: 'flex',
@@ -44,36 +40,35 @@ const Preport = (props) => {
         setAnchorEl(event.currentTarget)
     }
 
-    const CClose = () => {
+    const handlePrint = () => {
         setAnchorEl(null)
     }
 
     return props.trigger ? (
-        <>
+        
             <Dialog
                 open={props.trigger}
                 onClose={handleClose}
+                fullWidth
                 maxWidth="md"
                 aria-labelledby="scroll-dialog-title"
                 aria-describedby="scroll-dialog-description"
             >
-                <HorizonDialogTitle id="scroll-dialog-title">
-                    <HorizonDiv>
-                        <Button
-                            id="basic-button"
+                <DialogTitle sx={{display: 'flex', alignItems: 'center', padding: '.5rem 2rem'}}>
+                    
+                        <IconButton
                             aria-controls={open ? 'basic-menu' : undefined}
                             aria-haspopup="true"
                             aria-expanded={open ? 'true' : undefined}
                             onClick={handleClick}
                         >
-                            <AiIcons.AiOutlinePrinter style={{ fontSize: '2rem' }} />
-                        </Button>
+                            <Print />
+                        </IconButton>
                         <Dialog id="basic-menu" open={open}>
                             <Menu
                                 id="basic-menu-list"
                                 aria-labelledby="basic-button"
                                 open={open}
-                                onClose={CClose}
                                 anchorOrigin={{
                                     vertical: 'center',
                                     horizontal: 'center',
@@ -83,23 +78,26 @@ const Preport = (props) => {
                                     horizontal: 'center',
                                 }}
                             >
-                                <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={CClose}>
+                                <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={handlePrint}>
                                     正式報告
                                 </MenuItem>
-                                <MenuItem style={{ width: '10rem', height: '5rem' }} d onClick={CClose}>
+                                <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={handlePrint}>
                                     簡易報告
                                 </MenuItem>
                             </Menu>
                         </Dialog>
 
-                        <div>
-                            <Typography variant="h5">{info.patientID}/Apollo/Mr.</Typography>
-                            <Typography>Create:2023/6/2 PM 2:12:52</Typography>
-                            <Typography>Update:2023/6/2 PM 2:12:52</Typography>
-                        </div>
-                    </HorizonDiv>
-
-                    <FormControl sx={{ minWidth: 120 }}>
+                        <ListItemText
+                    primary={`${
+                        patient ? `${patient.id} / ${patient.name} / ${patient.gender === 'm' ? '先生' : '小姐'}` : '無病人資料'
+                    }`}
+                    secondary={
+                        <Box>
+                            <Box>{`建立 : ${new Date(info.report.createdAt).toLocaleString()}`}</Box>
+                            <Box>{`更新 : ${new Date(info.updatedAt).toLocaleString()}`}</Box>
+                        </Box>
+                    }
+                />
                         {info.status !== 'wait-examination' && (
                             <FormControl size="small" variant="outlined" sx={{ width: '5rem', ml: 4 }}>
                                 <InputLabel id="select-version">報告版本</InputLabel>
@@ -116,20 +114,31 @@ const Preport = (props) => {
                                 </Select>
                             </FormControl>
                         )}
-                    </FormControl>
-                </HorizonDialogTitle>
+                </DialogTitle>
                 <DialogContent>
-                    <DialogContentText
-                        id="scroll-dialog-description"
-                        ref={descriptionElementRef}
-                        tabIndex={-1}
-                    ></DialogContentText>
+                    <DialogContentText sx={{ height: '90vh', display: 'flex', justifyContent: 'center' }}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>項目</th>
+                                    <th>檢查結果及說明</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {reportdata.map((field) => (
+                                <tr>
+                                    <th>{field.label}</th>
+                                </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </DialogContentText>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
                 </DialogActions>
             </Dialog>
-        </>
+        
     ) : (
         ''
     )
