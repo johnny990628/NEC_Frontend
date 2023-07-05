0b0f832c31be4e97927bf78afce05f3827416ffc
ApolloChen <117019832+AdrianoXY@users.noreply.github.com>
2023-07-06 00:56:15 +0800

    update

--- src/Pages/Report/Preport.js
+++ src/Pages/Report/Preport.js
@@ -37,58 +37,65 @@ const Preport = (props) => {
         setAnchorEl(event.currentTarget)
     }
 
-    const formRef = useRef()
-    const handlePrint = useReactToPrint({
-        content: () => formRef.current,
+    const OfficialformRef = useRef()
+    const handleOfficialPrint = useReactToPrint({
+        content: () => OfficialformRef.current,
+    })
+
+    const SimpleformRef = useRef()
+    const handleSimplePrint = useReactToPrint({
+        content: () => SimpleformRef.current,
     })
 
     return props.trigger ? (
-        
-            <Dialog
-                open={props.trigger}
-                onClose={handleClose}
-                fullWidth
-                maxWidth="md"
-                aria-labelledby="scroll-dialog-title"
-                aria-describedby="scroll-dialog-description"
-            >
-                <DialogTitle sx={{display: 'flex', alignItems: 'center', padding: '.5rem 2rem'}}>
-                    
-                        <IconButton
-                            aria-controls={open ? 'basic-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                            onClick={handleClick}
-                        >
-                            <Print />
-                        </IconButton>
-                        <Dialog open={open}>
-                            <Menu
-                                id="basic-menu-list"
-                                aria-labelledby="basic-button"
-                                open={open}
-                                onClose={(e) => setAnchorEl(null)}
-                                anchorOrigin={{
-                                    vertical: 'center',
-                                    horizontal: 'center',
-                                }}
-                                transformOrigin={{
-                                    vertical: 'center',
-                                    horizontal: 'center',
-                                }}
-                            >
-                                <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={handlePrint}>
-                                    正式報告
-                                </MenuItem>
-                                <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={handlePrint}>
-                                    簡易報告
-                                </MenuItem>
-                            </Menu>
-                        </Dialog>
+        <Dialog
+            open={props.trigger}
+            onClose={handleClose}
+            fullWidth
+            maxWidth="md"
+            aria-labelledby="scroll-dialog-title"
+            aria-describedby="scroll-dialog-description"
+        >
+            <DialogTitle sx={{ display: 'flex', alignItems: 'center', padding: '.5rem 2rem' }}>
+                <IconButton
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
+                    <Print />
+                </IconButton>
+                <Dialog open={open}>
+                    <Menu
+                        id="basic-menu-list"
+                        aria-labelledby="basic-button"
+                        open={open}
+                        onClose={(e) => setAnchorEl(null)}
+                        anchorOrigin={{
+                            vertical: 'center',
+                            horizontal: 'center',
+                        }}
+                        transformOrigin={{
+                            vertical: 'center',
+                            horizontal: 'center',
+                        }}
+                    >
+                        <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={handleOfficialPrint}>
+                            正式報告
+                        </MenuItem>
+                        <MenuItem style={{ width: '10rem', height: '5rem' }} onClick={handleSimplePrint}>
+                            簡易報告
+                        </MenuItem>
+                    </Menu>
+                </Dialog>
 
-                        <ListItemText
+                <ListItemText
                     primary={`${
-                        info.patient ? `${info.patient.id} / ${info.patient.name} / ${info.patient.gender === 'm' ? '先生' : '小姐'}` : '無病人資料'
+                        info.patient
+                            ? `${info.patient.id} / ${info.patient.name} / ${
+                                  info.patient.gender === 'm' ? '先生' : '小姐'
+                              }`
+                            : '無病人資料'
                     }`}
                     secondary={
                         <Box>
@@ -97,47 +104,32 @@ const Preport = (props) => {
                         </Box>
                     }
                 />
-                        {info.status !== 'wait-examination' && (
-                            <FormControl size="small" variant="outlined" sx={{ width: '5rem', ml: 4 }}>
-                                <InputLabel id="select-version">報告版本</InputLabel>
-                                <Select
-                                    labelId="select-version"
-                                    label="報告版本"
-                                    value={version}
-                                    onChange={(e) => setVersion(e.target.value)}
-                                >
-                                    {info.report?.records &&
-                                        info.report?.records.map((record, index) => (
-                                            <MenuItem key={record.id} value={record.id}>{`v${index + 1}`}</MenuItem>
-                                        ))}
-                                </Select>
-                            </FormControl>
-                        )}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText sx={{ height: '90vh', display: 'flex', justifyContent: 'center' }}>
-                        <table ref={formRef}>
-                            <thead>
-                                <tr>
-                                    <th>項目</th>
-                                    <th>檢查結果及說明</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {reportdata.map((field) => (
-                                <tr>
-                                    <th>{field.label}</th>
-                                </tr>
+                {info.status !== 'wait-examination' && (
+                    <FormControl size="small" variant="outlined" sx={{ width: '5rem', ml: 4 }}>
+                        <InputLabel id="select-version">報告版本</InputLabel>
+                        <Select
+                            labelId="select-version"
+                            label="報告版本"
+                            value={version}
+                            onChange={(e) => setVersion(e.target.value)}
+                        >
+                            {info.report?.records &&
+                                info.report?.records.map((record, index) => (
+                                    <MenuItem key={record.id} value={record.id}>{`v${index + 1}`}</MenuItem>
                                 ))}
-                            </tbody>
-                        </table>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                </DialogActions>
-            </Dialog>
-        
+                        </Select>
+                    </FormControl>
+                )}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText
+                    sx={{ height: '90vh', display: 'flex', justifyContent: 'center' }}
+                ></DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+            </DialogActions>
+        </Dialog>
     ) : (
         ''
     )
