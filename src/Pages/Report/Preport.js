0e9db9c478d935d1e38b49b5eb6bd9ffb9f8e23a
AdrianoXY <117019832+AdrianoXY@users.noreply.github.com>
2023-07-06 14:54:39 +0800

    Print Report update

--- src/Pages/Report/Preport.js
+++ src/Pages/Report/Preport.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+
 import { styled } from '@mui/material/styles'
 import { IconButton, ListItemText } from '@mui/material'
 import { Close, Print } from '@mui/icons-material'
@@ -11,24 +12,23 @@ import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 import InputLabel from '@mui/material/InputLabel'
 import { FormControl, Menu, MenuItem, Select, Typography } from '@mui/material'
+
 import { useDispatch, useSelector } from 'react-redux'
+
 import { useReactToPrint } from 'react-to-print'
-import ReportFormHtml, { ReportFormForPDF } from '../../Components/ReportDialog/ReportFormHtml'
-import reportdata from '../../Assets/Json/ReportCols.json'
+
+import useStyles from './Style'
+import REPORTCOLS from '../../Assets/Json/ReportCols.json'
+import REPORTCOLS2 from '../../Assets/Json/ReportCols2.json'
 
 const Preport = (props) => {
     const { info } = props
     const dispatch = useDispatch()
     const [version, setVersion] = useState('')
-    const [report, setReport] = useState(reportdata)
     const descriptionElementRef = useRef(null)
     const [anchorEl, setAnchorEl] = useState(null)
     const open = Boolean(anchorEl)
-
-    const HorizonDiv = styled('div')({
-        display: 'flex',
-        flexDirection: 'row',
-    })
+    
 
     const handleClose = () => {
         props.setShowReport(false)
@@ -47,6 +47,261 @@ const Preport = (props) => {
     const handleSimplePrint = useReactToPrint({
         content: () => SimpleformRef.current,
     })
+
+    const ReportOfficialFormForPDF = React.forwardRef((_, ref) => {
+        return (
+            <div style={{ width: '100%' }} ref={ref}>
+                <FormHeader />
+                <PatientForm />
+                <ReportFormHtml print={true} />
+                <FormFooter />
+            </div>
+        )
+    })
+
+    const ReportSimpleFormForPDF = React.forwardRef((_, ref) => {
+        return (
+            <div style={{ width: '100%' }} ref={ref}>
+                <FormHeader />
+                <PatientForm />
+                <ReportFormHtml print={true} />
+                <FormFooter />
+            </div>
+        )
+    })
+
+    const FormHeader = () => {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <b style={{ fontSize: '1.5rem' }}>乳房超音波檢查報告單</b>
+                <hr></hr>
+                <div style={{ width: '90%', display: 'flex', justifyContent: 'space-between' }}>
+                    <div>檢查日期 : {new Date(info.report.createdAt).toLocaleDateString()}</div>
+                    <div>報告列印時間 : {new Date().toLocaleString()}</div>
+                </div>
+            </div>
+        )
+    }
+
+    const PatientForm = () => {
+        const classes = useStyles()
+        
+        const [patient, setPatient] = useState(info.patient)
+    
+        return (
+            <table className={classes.table} style={{ width: '90%', margin: 'auto' }}>
+                <tr>
+                    <td className={classes.table}>
+                        <b>姓名</b>
+                    </td>
+                    {patient ? (
+                        <td className={classes.table}>{patient?.name}</td>
+                    ) : (
+                        <td className={classes.table}>&emsp;&emsp;&emsp;</td>
+                    )}
+                    <td className={classes.table}>
+                        <b>性別</b>
+                    </td>
+                    <td className={classes.table}>
+                        <input type="checkbox" checked={patient?.gender === 'm'} readOnly />男
+                        <input type="checkbox" checked={patient?.gender === 'f'} readOnly />女
+                    </td>
+                    <td className={classes.table}>
+                        <b>出生年月日</b>
+                    </td>
+                    {patient ? (
+                        <td className={classes.table}>{new Date(patient?.birth).toLocaleDateString()}</td>
+                    ) : (
+                        <td className={classes.table}>&emsp;&emsp;&emsp;</td>
+                    )}
+                </tr>
+                <tr>
+                    <td className={classes.table}>
+                        <b>電話</b>
+                    </td>
+                    <td className={classes.table} colSpan="5">
+                        {patient?.phone}
+                    </td>
+                </tr>
+                <tr>
+                    <td className={classes.table}>
+                        <b>部門單位</b>
+                    </td>
+                    <td className={classes.table} colSpan="2">
+                        {patient?.department}
+                    </td>
+                    <td className={classes.table}>
+                        <b>身份證字號</b>
+                    </td>
+                    <td className={classes.table} colSpan="2">
+                        {patient?.id}
+                    </td>
+                </tr>
+            </table>
+        )
+    }
+
+    const ReportFormHtml = () => {
+        const classes = useStyles()
+        const report = useSelector((state) => state.reportForm.edit)
+        const [cancerArr, setCancerArr] = useState([])
+        useEffect(() => {
+            if (report) {
+                setCancerArr(report)
+            }
+        }, [report])
+    
+        return (
+            <table className={classes.table} style={{ width: '90%', margin: 'auto' }}>
+                <tbody>
+                    {[...REPORTCOLS, ...REPORTCOLS2].map((list, i) => (
+                        <>
+                            {i === 1 && (
+                                <tr>
+                                    <td
+                                        colSpan="4"
+                                        className={classes.table}
+                                        style={{ textAlign: 'center', height: '2rem' }}
+                                    >
+                                        <b>檢查適應症</b>
+                                    </td>
+                                </tr>
+                            )}
+                            {list.name === 'RADARMARK' && (
+                                <tr>
+                                    <td
+                                        colSpan="4"
+                                        className={classes.table}
+                                        style={{ textAlign: 'center', height: '2rem' }}
+                                    >
+                                        <b>{list.label}</b>
+                                    </td>
+                                </tr>
+                            )}
+                            <FormSection
+                                key={list.name}
+                                list={list}
+                                checked={cancerArr?.some((c) => c.name === list.name)}
+                                options={cancerArr?.find((c) => c.name === list.name)?.value}
+                                text={cancerArr?.find((c) => c.name === list.name)?.value}
+                            />
+                        </>
+                    ))}
+                </tbody>
+            </table>
+        )
+    }
+    
+    const FormSection = ({ list, checked, options, text }) => {
+        const classes = useStyles()
+        return list.section === 'Indication' ? (
+            <tr>
+                <td colSpan="4">
+                    {(list.type === 'radio' || list.type === 'select') && (
+                        <>
+                            <input type="checkbox" checked={checked} readOnly />
+                            <b>{list.label}</b>
+                            <>
+                                {list.options.map((option) => {
+                                    return (
+                                        <>
+                                            <input
+                                                type="radio"
+                                                value={option.value}
+                                                checked={options?.includes(option.value)}
+                                                readOnly
+                                            />
+                                            {option.label}
+                                        </>
+                                    )
+                                })}
+                            </>
+                        </>
+                    )}
+                    {list.type === 'checkbox' && (
+                        <>
+                            <input type="checkbox" checked={checked} readOnly />
+                            <b>{list.label}</b>
+                        </>
+                    )}
+                    {list.type === 'text' && (
+                        <>
+                            <input type="checkbox" checked={checked} readOnly />
+                            <b>{list.label} :</b> {text}
+                        </>
+                    )}
+                </td>
+            </tr>
+        ) : (
+            <tr className={`${classes.table} ${classes.printBreak}`}>
+                {(list.type === 'redio' || list.type === 'select') && (
+                    <>
+                        <td>
+                            <input type="checkbox" checked={checked} readOnly />
+                            <b>{list.label}</b>
+                        </td>
+    
+                        <td className={classes.table}>
+                            {list?.options?.map((option) => (
+                                <div>
+                                    <input
+                                        type="radio"
+                                        value={option.value}
+                                        checked={options?.includes(option.value)}
+                                        readOnly
+                                    />
+                                    {option.label}
+                                </div>
+                            ))}
+                        </td>
+                    </>
+                )}
+                {list.type === 'text' && (
+                    <>
+                        <td style={{ width: '10%', height: '3rem' }} className={classes.table}>
+                            <b>{list.label}</b>
+                        </td>
+                        <td style={{ width: '10%' }} className={classes.table}>
+                            {text}
+                        </td>
+                    </>
+                )}
+                {list.type === 'modal' && (
+                    <>
+                        {text && (
+                            <td colSpan="4" className={classes.table}>
+                                <div style={{ display: 'flex' }}>
+                                    {['R', 'L'].map((side) => {
+                                        return (
+                                            <div
+                                                style={{
+                                                    width: '50%',
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                {/* <DataShows azimut={JSON.parse(text)} side={side} /> */}
+                                                {/* <Chest azimut={JSON.parse(text)} side={side} /> */}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </td>
+                        )}
+                    </>
+                )}
+            </tr>
+        )
+    }
+
+    const FormFooter = () => {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <p style={{ fontSize: '1.2rem' }}></p>
+            </div>
+        )
+    }
 
     return props.trigger ? (
         <Dialog
@@ -126,7 +381,8 @@ const Preport = (props) => {
                 <DialogContentText sx={{ height: '90vh', display: 'flex', justifyContent: 'center' }}>
                     <ReportFormHtml />
                     <Box sx={{ display: 'none' }}>
-                        <ReportFormForPDF ref={OfficialformRef} />
+                        <ReportOfficialFormForPDF ref={OfficialformRef} />
+                        <ReportSimpleFormForPDF ref={SimpleformRef} />
                     </Box>
                 </DialogContentText>
             </DialogContent>
@@ -138,5 +394,7 @@ const Preport = (props) => {
         ''
     )
 }
+
+
 
 export default Preport
--- src/Pages/Report/Style.js
+++ src/Pages/Report/Style.js
@@ -61,6 +61,32 @@ const useStyles = makeStyles((theme) => ({
             color: theme.palette.status.purple_dark,
         },
     },
+    table: {
+        border: '1px solid #000000',
+        borderCollapse: 'collapse',
+        padding: '.15rem',
+        fontSize: '.8rem',
+    },
+    title: {
+        color: theme.palette.primary.main,
+        fontSize: '1rem',
+    },
+    patientInfo: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        color: theme.palette.primary.main,
+    },
+    actionButton: {
+        fontSize: '1rem',
+    },
+    printBreak: {
+        '@media print': {
+            // breakAfter: 'always !important',
+            // pageBreakAfter: 'always !important',
+            pageBreakInside: 'avoid !important',
+        },
+    },
 }))
 
 export default useStyles
